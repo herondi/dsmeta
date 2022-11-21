f8bb0a4a3017b1753179bdc74447bbdcd48cdca1
herondi <wayneramos93@gmail.com>
2022-11-20 21:09:15 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -28,7 +28,7 @@ function SalesCard() {
             .then(response => {
                 setSales(response.data.content);
             });
-    }, [minDate, maxDate])
+    }, [minDate, maxDate]);
 
 
 
